ab2e2a7f5606c96abe311b426120681abc68bfa4
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-23 09:54:25 +0430

    ui: add live service icons

--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -5,6 +5,14 @@
             <path d="M4 20 20 4l-4-4L0 16l4 4Z" transform="translate(2 2) scale(.83)" stroke="currentColor" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round"/>
             <path d="m8 14 2 2m1-5 2 2m1-5 2 2" stroke="currentColor" stroke-width="1.7" stroke-linecap="round"/>
             @break
+        @case('truck')
+            <path d="M14 18V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v11a1 1 0 0 0 1 1h2M15 18H9M19 18h2a1 1 0 0 0 1-1v-3.65a1 1 0 0 0-.22-.624l-3.48-4.35A1 1 0 0 0 17.52 8H14" stroke="currentColor" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round"/>
+            <circle cx="17" cy="18" r="2" stroke="currentColor" stroke-width="1.7"/>
+            <circle cx="7" cy="18" r="2" stroke="currentColor" stroke-width="1.7"/>
+            @break
+        @case('shield')
+            <path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1Z" stroke="currentColor" stroke-width="1.7" stroke-linejoin="round"/>
+            @break
         @case('globe')
             <circle cx="12" cy="12" r="9" stroke="currentColor" stroke-width="1.7"/>
             <path d="M3.5 12h17M12 3c2.5 2.5 3.7 5.5 3.7 9S14.5 18.5 12 21M12 3C9.5 5.5 8.3 8.5 8.3 12S9.5 18.5 12 21" stroke="currentColor" stroke-width="1.7" stroke-linecap="round"/>
