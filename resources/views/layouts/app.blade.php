862b5391635f9cb88bad5d5336b4e4b034519c6a
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-23 20:33:19 +0430

    ui: keep live CSS as reference without overriding Laravel utilities

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,9 +30,6 @@
         <script type="application/ld+json">{!! json_encode($seo->jsonLd, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) !!}</script>
     @endif
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @if (request()->routeIs('home'))
-        <link rel="stylesheet" href="{{ asset('css/kabulfit-live.css') }}">
-    @endif
 </head>
 <body>
 @php
