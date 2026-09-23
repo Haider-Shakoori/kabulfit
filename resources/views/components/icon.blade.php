f48c25bc07454e4171914e14ca2210ddd651768c
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-23 09:47:07 +0430

    ui: add live testimonial star icon

--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -62,6 +62,9 @@
             <circle cx="12" cy="12" r="4" stroke="currentColor" stroke-width="1.7"/>
             <circle cx="17.5" cy="6.8" r="1" fill="currentColor"/>
             @break
+        @case('star')
+            <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16Z" stroke="currentColor" stroke-width="1.7" stroke-linejoin="round"/>
+            @break
         @case('youtube')
             <path d="M21 8.1a3 3 0 0 0-2.1-2.2C17 5.4 12 5.4 12 5.4s-5 0-6.9.5A3 3 0 0 0 3 8.1 31 31 0 0 0 2.5 12 31 31 0 0 0 3 15.9a3 3 0 0 0 2.1 2.2c1.9.5 6.9.5 6.9.5s5 0 6.9-.5a3 3 0 0 0 2.1-2.2 31 31 0 0 0 .5-3.9 31 31 0 0 0-.5-3.9Z" stroke="currentColor" stroke-width="1.5" stroke-linejoin="round"/>
             <path d="m10 9 5 3-5 3V9Z" fill="currentColor"/>
